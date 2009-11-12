f13163015b73cafefd8fad2ad4a7dd9fd3a8e7c2
Adam Risi <ajrisi@gmail.com>
2009-11-11 22:01:26 -0500

    adding the code to get the hostname of the IP found

--- lsif.c
+++ lsif.c
@@ -86,7 +86,9 @@ int main(void)
   struct ifreq    *item;
   struct sockaddr *addr;
   struct sockaddr *broadcast_addr;
- 
+  socklen_t       salen;
+  char            hostname[1024];
+
   /* Get a socket handle. */
   sck = socket(PF_INET, SOCK_DGRAM, 0);
   if(sck < 0) {
@@ -104,112 +106,128 @@ int main(void)
   /* Iterate through the list of interfaces. */
   ifr = ifc.ifc_req;
   nInterfaces = ifc.ifc_len / sizeof(struct ifreq); 
-  for(i = 0; i < nInterfaces; i++)
-    {
-      item = &ifr[i];
- 
-      /* Show the device name and IP address */
-      addr = &(item->ifr_addr);
-      broadcast_addr = &(item->ifr_broadaddr);
+  for(i = 0; i < nInterfaces; i++) {
+    bzero(hostname, 1024);
+    item = &ifr[i];
+    
+    /* Show the device name and IP address */
+    addr = &(item->ifr_addr);
+    broadcast_addr = &(item->ifr_broadaddr);
+    
+    /* Get the address 
+     * This may seem silly but it seems to be needed on some systems 
+     */
+    if(ioctl(sck, SIOCGIFADDR, item) < 0) {
+      fatal_perror("ioctl(OSIOCGIFADDR)");
+    }
+    printf("%s\t%s",
+	   item->ifr_name,
+	   get_ip_str(addr, ip, INET6_ADDRSTRLEN));
+    
+    /* Get the broadcast address */
+    if(ioctl(sck, SIOCGIFBRDADDR, item) < 0) {
+      fatal_perror("ioctl(SIOCGIFBRDADDR)");	
+    }
+    
+    switch(addr->sa_family) {
+    case AF_INET:
+      salen = sizeof(struct sockaddr_in);
+      break;
+    case AF_INET6:
+      salen = sizeof(struct sockaddr_in6);
+      break;
+    default:
+      salen = 0;
+    }
+   
+    /* the call to get the mac address changes what is stored in the
+       item, meaning that we need to determine the hostname now */
+    getnameinfo(addr, salen, hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD);
 
-      /* Get the address 
-       * This may seem silly but it seems to be needed on some systems 
-       */
-      if(ioctl(sck, SIOCGIFADDR, item) < 0) {
-	fatal_perror("ioctl(OSIOCGIFADDR)");
-      }
-      printf("%s\t%s",
-	     item->ifr_name,
-	     get_ip_str(addr, ip, INET6_ADDRSTRLEN));
-      
-      /* Get the broadcast address */
-      if(ioctl(sck, SIOCGIFBRDADDR, item) < 0) {
-	fatal_perror("ioctl(SIOCGIFBRDADDR)");	
-      }
 
-      printf("\t%s", get_ip_str(broadcast_addr,ip,INET6_ADDRSTRLEN));
- 
-      /* Lots of different ways to get the ethernet address */
+    printf("\t%s", get_ip_str(broadcast_addr,ip,INET6_ADDRSTRLEN));
+    
+    /* Lots of different ways to get the ethernet address */
 #ifdef SIOCGIFHWADDR
-/* Linux */
-      /* Get the MAC address */
-      if(ioctl(sck, SIOCGIFHWADDR, item) < 0) {
-	fatal_perror("ioctl(SIOCGIFHWADDR)");
-	return 1;
-      }
- 
-      /* display result */
-      printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n",
-	     (unsigned char)item->ifr_hwaddr.sa_data[0],
-	     (unsigned char)item->ifr_hwaddr.sa_data[1],
-	     (unsigned char)item->ifr_hwaddr.sa_data[2],
-	     (unsigned char)item->ifr_hwaddr.sa_data[3],
-	     (unsigned char)item->ifr_hwaddr.sa_data[4],
-	     (unsigned char)item->ifr_hwaddr.sa_data[5]);
-
+    /* Linux */
+    /* Get the MAC address */
+    if(ioctl(sck, SIOCGIFHWADDR, item) < 0) {
+      fatal_perror("ioctl(SIOCGIFHWADDR)");
+      return 1;
+    }
+    
+    /* display result */
+    printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n",
+	   (unsigned char)item->ifr_hwaddr.sa_data[0],
+	   (unsigned char)item->ifr_hwaddr.sa_data[1],
+	   (unsigned char)item->ifr_hwaddr.sa_data[2],
+	   (unsigned char)item->ifr_hwaddr.sa_data[3],
+	   (unsigned char)item->ifr_hwaddr.sa_data[4],
+	   (unsigned char)item->ifr_hwaddr.sa_data[5]);
+    
 #elif SIOCGENADDR
-/* Solaris and possibly all SysVR4 */
-      /* Get the MAC address */
-      if(ioctl(sck, SIOCGENADDR, item) < 0) {
-	fatal_perror("ioctl(SIOCGENADDR)");
-      }
- 
-      /* display result */
-      printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n",
-	     (unsigned char)item->ifr_enaddr[0],
-	     (unsigned char)item->ifr_enaddr[1],
-	     (unsigned char)item->ifr_enaddr[2],
-	     (unsigned char)item->ifr_enaddr[3],
-	     (unsigned char)item->ifr_enaddr[4],
-	     (unsigned char)item->ifr_enaddr[5]);
-
+    /* Solaris and possibly all SysVR4 */
+    /* Get the MAC address */
+    if(ioctl(sck, SIOCGENADDR, item) < 0) {
+      fatal_perror("ioctl(SIOCGENADDR)");
+    }
+    
+    /* display result */
+    printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n",
+	   (unsigned char)item->ifr_enaddr[0],
+	   (unsigned char)item->ifr_enaddr[1],
+	   (unsigned char)item->ifr_enaddr[2],
+	   (unsigned char)item->ifr_enaddr[3],
+	   (unsigned char)item->ifr_enaddr[4],
+	   (unsigned char)item->ifr_enaddr[5]);
+    
 #elif __MACH__ || __NetBSD__ || __OpenBSD__ || __FreeBSD__
-/* MacOS X and all modern BSD implementations (I hope) */
-      int                mib[6] = {0};
-      int                len = 0;
-      char               *macbuf; 
-      struct if_msghdr   *ifm;
-      struct sockaddr_dl *sdl;
-      unsigned char      ptr[];
+    /* MacOS X and all modern BSD implementations (I hope) */
+    int                mib[6] = {0};
+    int                len = 0;
+    char               *macbuf; 
+    struct if_msghdr   *ifm;
+    struct sockaddr_dl *sdl;
+    unsigned char      ptr[];
+      
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    mib[5] = if_nametoindex(item->ifr_name);
+    if(mib[5] == 0)
+      continue;
 
-      mib[0] = CTL_NET;
-      mib[1] = AF_ROUTE;
-      mib[2] = 0;
-      mib[3] = AF_LINK;
-      mib[4] = NET_RT_IFLIST;
-      mib[5] = if_nametoindex(item->ifr_name);
-      if(mib[5] == 0)
-	continue;
+    if(sysctl(mib, 6, NULL, (size_t*)&len, NULL, 0) != 0) {
+      fatal_perror("sysctl");
+    }
 
-      if(sysctl(mib, 6, NULL, (size_t*)&len, NULL, 0) != 0) {
-	fatal_perror("sysctl");
-      }
+    macbuf = (char *) malloc(len);
+    if(macbuf == NULL) {
+      fprintf(stderr, "Unable to allocate necessary memory: %d\n", len);
+      exit(1);
+    }
 
-      macbuf = (char *) malloc(len);
-      if(macbuf == NULL) {
-	fprintf(stderr, "Unable to allocate necessary memory: %d\n", len);
-	exit(1);
-      }
+    if(sysctl(mib, 6, macbuf, (size_t*)&len, NULL, 0) != 0) {
+      fatal_perror("sysctl");
+    }
 
-      if(sysctl(mib, 6, macbuf, (size_t*)&len, NULL, 0) != 0) {
-	fatal_perror("sysctl");
-      }
+    ifm = (struct if_msghdr *)macbuf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
 
-      ifm = (struct if_msghdr *)macbuf;
-      sdl = (struct sockaddr_dl *)(ifm + 1);
-      ptr = (unsigned char *)LLADDR(sdl);
+    printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n", 
+	   ptr[0], ptr[1], ptr[2], 
+	   ptr[3], ptr[4], ptr[5]);
 
-      printf("\t%02x:%02x:%02x:%02x:%02x:%02x\n", 
-	     ptr[0], ptr[1], ptr[2], 
-	     ptr[3], ptr[4], ptr[5]);
-
-      free(macbuf);
+    free(macbuf);
       
 #else
 #error OS Distribution Not Recognized
 #endif
 
-    }
+  }
  
   return 0;
 }
